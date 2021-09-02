f9a1e5a83513cc7a7f8d951c64e3e92c720f6c07
rubelsarker0 <rubelsarker0@outlook.com>
2021-09-02 12:47:18 +0100

    added-alert-gui

--- js/book.js
+++ js/book.js
@@ -1,4 +1,6 @@
+// global variable and initialization
 const userInputField = document.getElementById('user-input-field');
+const totalResults = document.getElementById('total-result-found');
 
 const getElement = (id) => {
 	return document.getElementById(id);
@@ -11,20 +13,22 @@ const userInput = () => {
 	userInputField.value = '';
 };
 
+//Keyboard enter implementation
 userInputField.addEventListener('keyup', (event) => {
 	if (event.keyCode === 13 || event.keyCode === 'Enter') {
 		userInput();
 	}
 });
+
 // calling the openlibrary api to load the data
 const loadBooksData = async (userInput) => {
 	try {
 		showSpinner('spinner');
 		const booksContainer = getElement('books-container');
 		booksContainer.textContent = '';
-		getElement('total-result-found').classList.add('d-none');
+		totalResults.textContent = '';
 		if (userInput !== '') {
-			const bookUrl = `http://openlibrary.org/search.json?q=${userInput}`;
+			const bookUrl = `https://openlibrary.org/search.json?q=${userInput}`;
 			const response = await fetch(bookUrl);
 			const data = await response.json();
 			displayBookInfo(data);
@@ -37,17 +41,7 @@ const loadBooksData = async (userInput) => {
 	}
 };
 
-// show and hide spinner
-const hideSpinner = (id) => {
-	const spinner = getElement(id);
-	spinner.classList.add('d-none');
-};
-const showSpinner = (id) => {
-	const spinner = getElement(id);
-	spinner.classList.remove('d-none');
-};
 // displaying book details
-
 const displayBookInfo = (bookInfo) => {
 	const booksContainer = getElement('books-container');
 	booksContainer.textContent = '';
@@ -56,10 +50,11 @@ const displayBookInfo = (bookInfo) => {
 
 // total search result
 const totalSearchResultFound = (bookInfo, booksContainer) => {
-	const totalResults = getElement('total-result-found');
 	if (bookInfo.docs.length > 0) {
 		totalResults.classList.remove('d-none', 'text-center', 'text-danger');
-		totalResults.innerText = `#Result found: ${bookInfo.numFound}`;
+		const div = document.createElement('div');
+		div.innerHTML = totalResultAlertHtml(bookInfo);
+		totalResults.appendChild(div);
 
 		bookInfo.docs?.forEach((book) => {
 			const div = document.createElement('div');
@@ -71,17 +66,25 @@ const totalSearchResultFound = (bookInfo, booksContainer) => {
 	}
 };
 
-//Error mess function
+// show and hide spinner functions
+const hideSpinner = (id) => {
+	const spinner = getElement(id);
+	spinner.classList.add('d-none');
+};
+const showSpinner = (id) => {
+	const spinner = getElement(id);
+	spinner.classList.remove('d-none');
+};
+
+//Error message function
 const errorMessage = (errorMsg) => {
-	const totalContainer = getElement('total-result-found');
-	totalContainer.classList.remove('d-none');
-	totalContainer.classList.add('text-center', 'text-danger');
-	totalContainer.innerText = errorMsg;
+	totalResults.classList.remove('d-none');
+	totalResults.classList.add('text-center', 'text-danger', 'fs-5');
+	totalResults.innerText = errorMsg;
 };
 
 //generating book HTML to display details
 const generateBookHtml = (book) => {
-	// console.log(book);
 	const {
 		cover_i,
 		title,
@@ -94,11 +97,14 @@ const generateBookHtml = (book) => {
 		language,
 	} = book;
 
-	//conditions to check values
-	const bookImgUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
+	//conditions to check the values
+	const bookImgUrl = `https://covers.openlibrary.org/b/id/${
+		cover_i !== undefined ? cover_i : 6487762
+	}-M.jpg`;
+
 	const bookTitle = title ? title : title_suggest;
 	const authorName = book.hasOwnProperty('author_name')
-		? author_name?.join(', ')
+		? author_name[0]
 		: author_alternative_name;
 	const publisherName = publisher ? publisher[0] : 'N/A';
 	const firstPublishYear = first_publish_year ? first_publish_year : 'N/A';
@@ -111,7 +117,9 @@ const generateBookHtml = (book) => {
             <div class="card-body p-1">
                 <h5 class="card-title text-primary">Title: ${bookTitle}</h5>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><strong>Author:</strong> ${authorName}</li>
+                    <li class="list-group-item"><strong>Author:</strong> ${
+											authorName || 'N/A'
+										}</li>
                     <li class="list-group-item"><strong>Publisher:</strong> ${publisherName}</li>
                     <li class="list-group-item"><strong>Publish Date:</strong> ${publishDate}</li>
                     
@@ -129,5 +137,21 @@ const generateBookHtml = (book) => {
 					</div>
 				</div>
 			</div>
+        </div>`;
+};
+
+const totalResultAlertHtml = (alertMsg) => {
+	return `
+        <div class="alert border-0 border-start border-5 border-primary alert-dismissible fade show py-2">
+            <div class="d-flex align-items-center">
+                <div class="font-35 text-primary"><i class="fas fa-heartbeat fa-2x"></i>
+                
+                </div>
+                <div class="ms-3">
+                    <h6 class="mb-0 text-primary fs-5">#Result found: ${alertMsg.numFound}</h6>
+                    <div>Successfully loaded!</i></div>
+                </div>
+            </div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>`;
 };
